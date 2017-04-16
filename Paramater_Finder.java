1ca3bb477dd70f2bc8144e8e9476257fe3bacbba
gurchetan sidhu <gurchetanbir.sidhu@sjsu.edu>
2017-04-15 22:38:53 -0700

    to find parameters

---
+++ Paramater_Finder.java
@@ -0,0 +1,14 @@
+import com.github.javaparser.ast.type.Type;
+
+public class Paramater_Finder {
+
+    private void checkRefrenceOrCollectionType(Type attributeType) {
+
+    }
+
+    private void checkIsPrimitiveType(Type attrType) {
+
+    }
+
+
+}
