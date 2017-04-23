620dbb53a4a35a543d73fccdee660003fc57a797
gurchetan sidhu <gurchetanbir.sidhu@sjsu.edu>
2017-04-22 22:31:58 -0700

    java main file

--- MainJavaFILE.java
+++ MainJavaFILE.java
@@ -4,20 +4,31 @@ import java.io.IOException;
 
 import net.sourceforge.plantuml.SourceStringReader;
 
-public class MainJavaFILE {
+
+
+
+public class Main_Class {
 	public static void main (String args[])
 	{
 		if(args!= null)
 		{
 		
-			
+			try
+			{
 				CodeParser codeparser = new CodeParser();
 				StringBuilder stringbuilder = codeparser.Read("/Users/chetansidhu/Desktop/A");
 				System.out.println(stringbuilder.toString());
 				SourceStringReader plantUmlReader = new SourceStringReader(stringbuilder.toString());
-				(FileOutputStream imageOutputStream = new FileOutputStream("/Users/chetansidhu/Desktop/umldiagram.png")) {
+				try(FileOutputStream imageOutputStream = new FileOutputStream("/Users/chetansidhu/Desktop/umldiagram.png")) {
 	                plantUmlReader.generateImage(imageOutputStream);
-	           
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+			}
+			catch(Exception e)
+			{
+				System.out.println("Error");
+			}
 		}
 	}
 
